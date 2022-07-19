ab7fd31a69bc186c4a2957a1d7b15a9746e28f42
ittehadzihan <zihan991@gmail.com>
2022-07-19 15:41:23 -0400

    DONE

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName (){
+   customerName = customerName.toUpperCase()
+}
+var bestCustomer;
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'Zihan'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Anika'
+}
